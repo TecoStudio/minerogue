db9679c1237c367a72165c1e157df9590de80670
LIPiston <wps1910093789@gmail.com>
2026-07-20 04:16:29 +0800

    feat: update default mobs

--- src/main/java/com/roguelike/config/DefaultMobs.java
+++ src/main/java/com/roguelike/config/DefaultMobs.java
@@ -8,112 +8,41 @@ public final class DefaultMobs {
     }
 
     public static int defaultExperience() {
-        return 10;
+        return 0;
     }
 
     public static boolean internalSystemEnabled() {
-        return true;
+        return false;
     }
 
     public static double skeletonEliteSpawnChance() {
-        return 0.12;
+        return 0.0;
     }
 
     public static ConfigManager.SkeletonEliteConfig skeletonElite() {
         return new ConfigManager.SkeletonEliteConfig(
-                true,
-                0.12,
-                "&c骷髅精英",
-                30.0,
-                5.0,
-                0.30,
-                0.10,
-                5.0,
-                "rusty_iron_sword",
-                18.0,
-                8.0,
-                3.2,
-                35L,
-                100L,
-                1.9,
-                0.22,
-                0.75,
-                0.65
+                false, 0.0, "", 20.0, 0.0, 0.0, 0.0, 0.0, null,
+                0.0, 0.0, 0.0, 20L, 20L, 1.0, 0.0, 0.0, 0.0, ""
         );
     }
 
     public static ConfigManager.ZombieEliteConfig zombieElite() {
-        return new ConfigManager.ZombieEliteConfig(
-                true,
-                0.12,
-                "&2僵尸精英",
-                35.0,
-                5.0,
-                "excited_stone_sword"
-        );
+        return new ConfigManager.ZombieEliteConfig(false, 0.0, "", 20.0, 0.0, null, "");
     }
 
     public static ConfigManager.SpiderEliteConfig spiderElite() {
-        return new ConfigManager.SpiderEliteConfig(
-                true,
-                0.12,
-                "&5精英蜘蛛",
-                35.0,
-                1.2,
-                0.35,
-                8.0,
-                1
-        );
+        return new ConfigManager.SpiderEliteConfig(false, 0.0, "", 20.0, 1.0, 0.0, 0.0, 1, "");
     }
 
-    public static ConfigManager.BossConfig conciergeBoss() {
-        return new ConfigManager.BossConfig(
-                true,
-                "&4沸血僵尸",
-                180.0,
-                9.0,
-                0.95,
-                24.0,
-                5.0,
-                120L,
-                8.0
-        );
-    }
-
-    public static ConfigManager.BossConfig timeKeeperBoss() {
-        return new ConfigManager.BossConfig(
-                true,
-                "&5流浪者",
-                150.0,
-                7.0,
-                1.25,
-                26.0,
-                4.5,
-                90L,
-                6.0
-        );
+    public static ConfigManager.ScriptedMobConfig scriptedMob() {
+        return new ConfigManager.ScriptedMobConfig(false, "", 1.0, 0.0, 1.0, 0.0, 0.0, 20L, 0.0);
     }
 
     public static Map<String, Integer> experience() {
-        Map<String, Integer> experience = new LinkedHashMap<>();
-        experience.put("zombie", 15);
-        experience.put("skeleton", 15);
-        experience.put("creeper", 20);
-        experience.put("spider", 12);
-        experience.put("enderman", 30);
-        experience.put("blaze", 22);
-        experience.put("warden", 350);
-        experience.put("ender_dragon", 500);
-        experience.put("wither", 300);
-        return experience;
+        return new LinkedHashMap<>();
     }
 
     public static Map<String, ConfigManager.MobConfig> modifiers() {
-        Map<String, ConfigManager.MobConfig> modifiers = new LinkedHashMap<>();
-        modifiers.put("husk", new ConfigManager.MobConfig(1.25, 1.10, 0.95, "frost_cleaver"));
-        modifiers.put("drowned", new ConfigManager.MobConfig(1.10, 1.05, 1.05, "storm_spear"));
-        modifiers.put("pillager", new ConfigManager.MobConfig(1.15, 1.15, 1.00, "echo_blade"));
-        modifiers.put("zombified_piglin", new ConfigManager.MobConfig(1.20, 1.20, 1.05, "plague_saber"));
-        return modifiers;
+        return new LinkedHashMap<>();
     }
 }
