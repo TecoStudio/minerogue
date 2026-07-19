5b2655395bac795aa4986d813a082ad59adb9e54
LIPiston <wps1910093789@gmail.com>
2026-07-20 04:16:24 +0800

    feat: update roguelike plugin

--- src/main/java/com/roguelike/RoguelikePlugin.java
+++ src/main/java/com/roguelike/RoguelikePlugin.java
@@ -12,11 +12,13 @@ import com.roguelike.forge.ForgeRecipeManager;
 import com.roguelike.integration.IntegrationManager;
 import com.roguelike.level.LevelManager;
 import com.roguelike.listener.EventListener;
+import com.roguelike.mana.ManaManager;
 import com.roguelike.mob.MobManager;
 import com.roguelike.scoreboard.RoguelikeScoreboard;
 import com.roguelike.ticket.TicketManager;
 import com.roguelike.util.DevLog;
 import com.roguelike.weapon.ToolAbilityManager;
+import com.roguelike.weapon.BowAbilityManager;
 import com.roguelike.weapon.WeaponAbilityManager;
 import com.roguelike.weapon.WeaponManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -38,7 +40,9 @@ public class RoguelikePlugin extends JavaPlugin {
         TicketManager.init(this);
         MobManager.init(this);
         LevelManager.init(this);
+        ManaManager.init(this);
         CombatHandler.init(this);
+        BowAbilityManager.init(this);
         WeaponAbilityManager.init(this);
         ToolAbilityManager.init(this);
         ArmorSetManager.init(this);
@@ -62,7 +66,9 @@ public class RoguelikePlugin extends JavaPlugin {
     public void onDisable() {
         MobManager.shutdown();
         WeaponAbilityManager.shutdown();
+        BowAbilityManager.shutdown();
         RoguelikeScoreboard.shutdown();
+        ManaManager.shutdown();
         PlayerDataManager.shutdown();
         DevLog.info("Roguelike plugin disabled.");
     }
